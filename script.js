602298248d0a76f7e9545f0f1b458a908eb86306
sivakumar D <162899443+webdsk@users.noreply.github.com>
2024-10-03 19:10:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+// const bg = document.querySelector('.bg')
+// const back=document.querySelector('.back')
+
+// let load=0;
+
+// let interval=setInterval(siva,30)
+
+// function siva(){
+//     load++
+
+//     if (load > 99) {
+//         clearInterval(interval)
+
+//     }
+
+//     back.innerText = `${load}%`
+//     back.style.opacity= scale(load,0,100,1,0)
+//     bg.style.filter=`blur(${scale(load,0,100,30,0)}px)`
+// }
+// function scale (number, inMin, inMax, outMin, outMax) {
+//     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+// }
+
+
+
+const bg=document.querySelector('.bg')
+const back=document.querySelector('.back')
+
+let load=0;
+
+let int=setInterval(siva,30)
+
+
+function siva(){
+    load++
+
+    console.log(load);
+    
+    if (load > 99) {
+
+        clearInterval(int)
+        
+    }
+    back.innerText=`${load}%`
+    back.style.opacity=scale(load,0,100,1,0)
+    bg.style.filter=`blur(${scale(load,0,100,30,0)}px)`
+
+
+
+}
+    function scale (number, inMin, inMax, outMin, outMax) {
+         return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+ }
+
+
+//  back.innerText=`${load}%`
+//  back.style.opacity=scale(load,0,100,1,0)
+//  bg.style.filter=`blur(${scale(0,100,30,0)}px)`
